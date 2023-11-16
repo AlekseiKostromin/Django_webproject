c87f83b0ac4bf3cd2d5ac6646ed74d9b28baf238
AlekseiKostromin <cs777@yandex.ru>
2023-11-16 14:46:21 +0500

    Add static and correction errores

--- config/settings.py
+++ config/settings.py
@@ -121,7 +121,7 @@ STATIC_URL = 'static/'
 
 STATICFILES_DIRS = (
     BASE_DIR / 'static',
-)
+)  # Статические файлы
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
--- config/urls.py
+++ config/urls.py
@@ -16,12 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-
 from catalog.views import index, contacts
 
-
-
 urlpatterns = [
-    path('', index),
-    path('contacts/', contacts),
+    path('admin/', admin.site.urls),
+    path('', include('catalog.urls')),
+    path('contacts/', include('catalog.urls')),
 ]
